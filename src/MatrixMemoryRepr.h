74faac7fa0cfbd149445fe4d9449e1c15f04a1b5
Josef Melcr <melcrjos@fit.cvut.cz>
2023-04-23 18:39:49 +0200

    add MatrixMemoryRepr header file

---
+++ src/MatrixMemoryRepr.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#ifndef MELCRJOS_MATRIXMEMORYREPR_H
+#define MELCRJOS_MATRIXMEMORYREPR_H
+
+#include "MatrixElement.h"
+#include <cstdlib>
+#include <iostream>
+#include <optional>
+#include <vector>
+
+class MatrixMemoryRepr {
+  public:
+    MatrixMemoryRepr() = delete;
+    MatrixMemoryRepr(std::size_t rows, std::size_t columns);
+    virtual ~MatrixMemoryRepr() = default;
+    virtual MatrixMemoryRepr * clone() const = 0;
+    friend std::ostream & operator<<(ostream &, const MatrixMemoryRepr &);
+
+    virtual int det() = 0;
+    virtual int det() const = 0;
+    virtual int rank() = 0;
+    virtual int rank() const = 0;
+    virtual void gem() = 0;
+    virtual MatrixMemoryRepr * gem() const = 0;
+    virtual void inverse() = 0;
+    virtual MatrixMemoryRepr * inverse() const = 0;
+    virtual void transpose() = 0;
+    virtual MatrixMemoryRepr * transpose() const = 0;
+    virtual std::vector<MatrixElement> dump() const = 0;
+    virtual void unite(const MatrixMemoryRepr &) = 0;
+    virtual MatrixMemoryRepr * unite(const MatrixMemoryRepr &) const = 0;
+
+  protected:
+    std::size_t _rows;
+    std::size_t _columns;
+    std::optional<int> _det;
+    std::optional<int> _rank;
+    virtual void print(std::ostream &) const = 0;
+};
+
+#endif // MELCRJOS_MATRIXMEMORYREPR_H
