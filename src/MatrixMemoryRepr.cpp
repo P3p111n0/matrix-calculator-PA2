8b9052a1bc69e3bb7cc85414d8d8a8894d3863b9
Josef Melcr <melcrjos@fit.cvut.cz>
2023-04-25 10:32:21 +0200

    add dbl_cmp method

--- src/MatrixMemoryRepr.cpp
+++ src/MatrixMemoryRepr.cpp
@@ -1,4 +1,5 @@
 #include "MatrixMemoryRepr.h"
+#include <limits>
 
 MatrixMemoryRepr::MatrixMemoryRepr(std::size_t rows, std::size_t columns)
     : _rows(std::move(rows)), _columns(std::move(columns)) {}
@@ -11,3 +12,10 @@ std::ostream & operator<<(std::ostream & os, const MatrixMemoryRepr & matrix) {
 size_t MatrixMemoryRepr::rows() const { return _rows; }
 
 size_t MatrixMemoryRepr::columns() const { return _columns; }
+
+bool MatrixMemoryRepr::dbl_cmp(double lhs, double rhs) {
+    constexpr int magic_constant = 420;
+    return abs(abs(lhs) - abs(rhs)) <=
+           magic_constant * std::numeric_limits<double>::epsilon() *
+               (abs(lhs) + abs(rhs));
+}
