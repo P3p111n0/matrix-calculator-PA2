5bcd6059e084ed6bc1b93fa0161d2b38fc6dc44d
Josef Melcr <melcrjos@fit.cvut.cz>
2023-04-26 20:15:46 +0200

    change SparseMatrix impl to fit MatrixMemoryRepr

--- src/MatrixMemoryRepr.h
+++ src/MatrixMemoryRepr.h
@@ -16,10 +16,10 @@ class MatrixMemoryRepr {
     friend std::ostream & operator<<(std::ostream &, const MatrixMemoryRepr &);
     std::size_t rows() const;
     std::size_t columns() const;
-    Rational at(std::size_t, std::size_t) const;
-    void add(std::size_t, std::size_t, const Rational &);
-    Rational & modify(std::size_t, std::size_t);
-    void swap_rows(std::size_t, std::size_t);
+    virtual Rational at(std::size_t, std::size_t) const = 0;
+    virtual void add(std::size_t, std::size_t, const Rational &) = 0;
+    virtual void modify(std::size_t, std::size_t, const Rational &) = 0;
+    virtual void swap_rows(std::size_t, std::size_t) = 0;
 
     virtual std::vector<std::vector<Rational>> dump() const = 0;
 
--- src/SparseMatrix.h
+++ src/SparseMatrix.h
@@ -1,30 +1,25 @@
 #include "MatrixMemoryRepr.h"
-#include <set>
-#include <initializer_list>
-#include <vector>
-#include <functional>
 #include "Rational.h"
+#include <functional>
+#include <initializer_list>
+#include <set>
+#include <vector>
 
 #ifndef MELCRJOS_SPARSEMATRIX_H
 #define MELCRJOS_SPARSEMATRIX_H
 
 class SparseMatrix : public MatrixMemoryRepr {
     using MemoryDump = std::vector<std::vector<Rational>>;
+
   public:
     SparseMatrix(size_t, size_t);
     SparseMatrix(const MemoryDump &);
     SparseMatrix(std::initializer_list<std::initializer_list<Rational>>);
-
     MatrixMemoryRepr * clone() const override;
-
-    Rational det() override;
-    Rational det() const override;
-    std::size_t rank() override;
-    std::size_t rank() const override;
-    void gem() override;
-    void inverse() override;
-    void transpose() override;
-    void unite(const MatrixMemoryRepr &) override;
+    Rational at(std::size_t, std::size_t) const override;
+    void add(std::size_t, std::size_t, const Rational &) override;
+    void modify(std::size_t, std::size_t, const Rational &) override;
+    void swap_rows(std::size_t, std::size_t) override;
     std::vector<std::vector<Rational>> dump() const override;
 
   protected:
@@ -34,19 +29,13 @@ class SparseMatrix : public MatrixMemoryRepr {
     struct MatrixElement {
         MatrixElement() = delete;
         MatrixElement(std::size_t r, std::size_t col, const Rational & val)
-                : row(r), column(col), value(val) {}
+            : row(r), column(col), value(val) {}
         std::size_t row;
         std::size_t column;
         Rational value;
         inline bool operator<(const MatrixElement & rhs) const;
     };
     std::set<MatrixElement> _data;
-
-    Rational calc_det() const;
-    size_t calc_rank() const;
-    void memory_dump_to_data(const MemoryDump &);
-    static void gem_swap_lines(MemoryDump &, std::function<void(std::size_t, std::size_t, Rational)> &&);
-    static void gem_row_elim(MemoryDump &, std::function<void(std::size_t, std::size_t, Rational)> &&);
 };
 
 #endif // MELCRJOS_SPARSEMATRIX_H
