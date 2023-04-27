5081530c915dcd09b7b0b484eee2e92dd57ab691
Josef Melcr <melcrjos@fit.cvut.cz>
2023-04-27 18:28:30 +0200

    replace set with map

--- src/SparseMatrix.cpp
+++ src/SparseMatrix.cpp
@@ -1,20 +1,13 @@
 #include "SparseMatrix.h"
 #include "Rational.h"
+#include "MatrixElement.h"
+
+bool SparseMatrix::Position::operator<(const SparseMatrix::Position & rhs) const {
+    return row == rhs.row ? col < rhs.col : row < rhs.row;
+}
 
 SparseMatrix::SparseMatrix(std::size_t r, std::size_t c)
     : MatrixMemoryRepr(r, c) {}
-
-SparseMatrix::SparseMatrix(const SparseMatrix::MemoryDump & mem)
-    : MatrixMemoryRepr(mem.size(), mem.size() ? mem.at(0).size() : 0) {
-
-    for (std::size_t i = 0; i < _rows; i++) {
-        for (std::size_t j = 0; j < _columns; j++) {
-            if (mem[i][j] != 0) {
-                _data.emplace(i, j, mem[i][j]);
-            }
-        }
-    }
-}
 
 SparseMatrix::SparseMatrix(
     std::initializer_list<std::initializer_list<Rational>> init_list)
@@ -26,7 +19,7 @@ SparseMatrix::SparseMatrix(
         std::size_t col = 0;
         for (const auto & val : list) {
             if (val != 0) {
-                _data.emplace(row, col, val);
+                _data[{row, col}] = val;
             }
             ++col;
         }
@@ -39,81 +32,49 @@ MatrixMemoryRepr * SparseMatrix::clone() const {
 }
 
 Rational SparseMatrix::at(std::size_t row, std::size_t column) const {
-    MatrixElement dummy(row, column, 0);
-    if (!_data.count(dummy)) {
+    if (!_data.count({row, column})) {
         return 0;
     }
-    return _data.find(dummy)->value;
+    return _data.at({row, column});
 }
 
 void SparseMatrix::add(std::size_t row, std::size_t column,
                        const Rational & val) {
 
-    auto req_element = _data.find(MatrixElement(row, column, 0));
-    if (req_element == _data.end()) {
-        _data.emplace(row, column, val);
-        return;
-    }
-
-    Rational found_val = req_element->value;
-    _data.erase(req_element);
-    found_val += val;
-    _data.emplace(row, column, found_val);
+    _data[{row, column}] += val;
 }
 
 void SparseMatrix::modify(std::size_t row, std::size_t column,
                           const Rational & new_val) {
-    auto element_to_modify = _data.find(MatrixElement(row, column, 0));
-    if (element_to_modify == _data.end()) {
-        _data.emplace(row, column, new_val);
-        return;
+    if (new_val != 0){
+        _data[{row, column}] = new_val;
     }
-    _data.erase(element_to_modify);
-    _data.emplace(row, column, new_val);
 }
 
 void SparseMatrix::swap_rows(std::size_t f_row, std::size_t s_row) {
     std::vector<MatrixElement> swap_elements;
 
-    for (const auto & i : _data) {
-        if (i.row == f_row || i.row == s_row) {
-            swap_elements.emplace_back(i);
+    for (const auto & [key, value] : _data) {
+        if (key.row == f_row || key.row == s_row) {
+            swap_elements.emplace_back(key.row, key.col, value);
         }
     }
     for (const auto & element : swap_elements) {
-        auto erase_it = _data.find(element);
-        _data.erase(erase_it);
-        _data.emplace(element.row == f_row ? s_row : f_row, element.column,
-                      element.value);
+        Position new_pos = {element.row == f_row ? s_row : f_row, element.column};
+        _data[new_pos] = element.value;
     }
-}
-
-std::vector<std::vector<Rational>> SparseMatrix::dump() const {
-    std::vector<std::vector<Rational>> result;
-    for (std::size_t i = 0; i < _rows; i++) {
-        std::vector<Rational> row;
-        for (std::size_t j = 0; j < _columns; j++) {
-            row.emplace_back(0);
-        }
-        result.emplace_back(row);
-    }
-    for (const auto & e : _data) {
-        result[e.row][e.column] = e.value;
-    }
-
-    return result;
 }
 
 void SparseMatrix::print(std::ostream & os) const {
     for (std::size_t i = 0; i < _rows; i++) {
         os << "[ ";
         for (std::size_t j = 0; j < _columns - 1; j++) {
-            auto elem = _data.find(MatrixElement(i, j, 0));
-            Rational val = elem == _data.end() ? 0 : elem->value;
+            auto elem = _data.find({i, j});
+            Rational val = elem == _data.end() ? 0 : elem->second;
             os << val << ", ";
         }
-        auto last_elem = _data.find(MatrixElement(i, _columns - 1, 0));
-        Rational last_val = last_elem == _data.end() ? 0 : last_elem->value;
+        auto last_elem = _data.find({i, _columns - 1});
+        Rational last_val = last_elem == _data.end() ? 0 : last_elem->second;
         os << last_val << " ]";
         if (i != _rows - 1) {
             os << std::endl;
--- src/SparseMatrix.h
+++ src/SparseMatrix.h
@@ -1,41 +1,36 @@
+#pragma once
+
 #include "MatrixMemoryRepr.h"
 #include "Rational.h"
 #include <functional>
 #include <initializer_list>
-#include <set>
+#include <map>
 #include <vector>
 
 #ifndef MELCRJOS_SPARSEMATRIX_H
 #define MELCRJOS_SPARSEMATRIX_H
 
 class SparseMatrix : public MatrixMemoryRepr {
-    using MemoryDump = std::vector<std::vector<Rational>>;
 
   public:
     SparseMatrix(size_t, size_t);
-    SparseMatrix(const MemoryDump &);
     SparseMatrix(std::initializer_list<std::initializer_list<Rational>>);
     MatrixMemoryRepr * clone() const override;
     Rational at(std::size_t, std::size_t) const override;
     void add(std::size_t, std::size_t, const Rational &) override;
     void modify(std::size_t, std::size_t, const Rational &) override;
     void swap_rows(std::size_t, std::size_t) override;
-    std::vector<std::vector<Rational>> dump() const override;
 
   protected:
     void print(std::ostream &) const override;
 
   private:
-    struct MatrixElement {
-        MatrixElement() = delete;
-        MatrixElement(std::size_t r, std::size_t col, const Rational & val)
-            : row(r), column(col), value(val) {}
+    struct Position {
         std::size_t row;
-        std::size_t column;
-        Rational value;
-        inline bool operator<(const MatrixElement & rhs) const;
+        std::size_t col;
+        inline bool operator<(const Position &) const;
     };
-    std::set<MatrixElement> _data;
+     std::map<Position, Rational> _data;
 };
 
 #endif // MELCRJOS_SPARSEMATRIX_H
