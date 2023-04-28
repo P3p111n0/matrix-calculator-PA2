9ce633c00716c628959b7ea702b5cc1fec0c922e
Josef Melcr <melcrjos@fit.cvut.cz>
2023-04-28 10:15:27 +0200

    add definitions for IteratorWrapper methods

--- src/IteratorWrapper.cpp
+++ src/IteratorWrapper.cpp
@@ -1,5 +1,19 @@
-//
-// Created by pjepjeno on 4/28/23.
-//
-
 #include "IteratorWrapper.h"
+
+IteratorWrapper::IteratorWrapper(const AbstractMatrixIterator *) {}
+
+void IteratorWrapper::operator++() {}
+
+MatrixElement IteratorWrapper::operator*() const { return MatrixElement(); }
+
+std::size_t IteratorWrapper::operator-(const IteratorWrapper &) const {
+    return 0;
+}
+
+bool IteratorWrapper::operator==(const IteratorWrapper &) const {
+    return false;
+}
+
+bool IteratorWrapper::operator!=(const IteratorWrapper &) const {
+    return false;
+}
