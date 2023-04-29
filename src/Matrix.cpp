63f134d09e35ac8d3c7b4c488c541948ee299d88
Josef Melcr <melcrjos@fit.cvut.cz>
2023-04-29 14:52:52 +0200

    add matrix inversion

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,16 +1,19 @@
 #include "Matrix.h"
+#include <queue>
+#include <set>
 
 constexpr auto default_capture = [](std::size_t, std::size_t) { return; };
 
-void Matrix::gem_swap_rows(
-    std::function<void(std::size_t, std::size_t)> && capture_fn = default_capture) {
+void Matrix::gem_swap_rows(std::function<void(std::size_t, std::size_t)> &&
+                               capture_fn = default_capture) {
 
     for (std::size_t i = 0, column_index = 0;
          i < rows() && column_index < columns(); i++, column_index++) {
         if (_matrix->at(i, column_index).value() == 0) {
             for (std::size_t j = i; j < rows(); j++) {
                 for (std::size_t h = column_index; h < columns(); h++) {
-                    if (_matrix->at(i, h).value() == 0 && _matrix->at(j, h).value() != 0) {
+                    if (_matrix->at(i, h).value() == 0 &&
+                        _matrix->at(j, h).value() != 0) {
                         _matrix->swap_rows(i, j);
                         capture_fn(i, j);
                         break;
@@ -22,8 +25,8 @@ void Matrix::gem_swap_rows(
 }
 
 // https://www.math-cs.gordon.edu/courses/ma342/handouts/gauss.pdf
-void Matrix::gem_row_elim(
-    std::function<void(std::size_t, std::size_t)> && capture_fn = default_capture) {
+void Matrix::gem_row_elim(std::function<void(std::size_t, std::size_t)> &&
+                              capture_fn = default_capture) {
 
     for (std::size_t i = 0; i < rows() - 1; i++) {
         for (std::size_t j = i + 1; j < rows(); j++) {
@@ -155,13 +158,97 @@ void Matrix::unite(const Matrix & other) {
 
 void Matrix::cut(std::size_t) {}
 
-void Matrix::inverse() {}
+void Matrix::inverse() {
+    if (rows() != columns()) {
+        throw std::logic_error("Non-square matrices cannot be inverted.");
+    }
+    if (_det.has_value() && _det.value() == 0) {
+        throw std::logic_error("Matrix is not invertible.");
+    }
+
+    std::queue<std::size_t> index_queue;
+    std::set<std::size_t> visited_indexes;
+    Rational determinant = 1;
+    std::size_t dim = rows();
+    bool is_transposed = false;
+
+    // check, if matrix should be transposed
+    std::size_t missing_diagonal_el = 0;
+    for (std::size_t i = 0; i < dim; i++) {
+        if (_matrix->at(i, i).value() == 0) {
+            missing_diagonal_el++;
+            continue;
+        }
+        break;
+    }
+
+    if (missing_diagonal_el == dim) {
+        transpose();
+        is_transposed = true;
+    }
+
+    for (std::size_t i = 0; i < dim; i++) {
+        index_queue.emplace(i);
+    }
+
+    std::vector<std::pair<std::size_t, std::size_t>> row_swap_vec;
+    gem_swap_rows(
+        [&](std::size_t i, std::size_t j) { row_swap_vec.emplace_back(i, j); });
+
+    for (std::size_t pivot_loc = index_queue.front(); !index_queue.empty();
+         index_queue.pop(), pivot_loc = index_queue.front()) {
+        Rational pivot = _matrix->at(pivot_loc, pivot_loc).value();
+        determinant *= pivot; // step 3
+        if (pivot == 0) {
+            if (!visited_indexes.count(pivot_loc)) {
+                index_queue.emplace(pivot_loc);
+                visited_indexes.emplace(pivot_loc);
+                continue;
+            }
+            _det = 0;
+            throw std::logic_error("Matrix is not invertible.");
+        }
+        // step 6
+        for (std::size_t pivot_column = 0; pivot_column < dim; pivot_column++) {
+            _matrix->modify(pivot_column, pivot_loc,
+                            _matrix->at(pivot_column, pivot_loc).value() /
+                                (Rational(-1) * pivot));
+        }
+        // step 7
+        for (std::size_t j = 0; j < dim; j++) {
+            if (pivot_loc != j) {
+                for (std::size_t k = 0; k < dim; k++) {
+                    if (pivot_loc != k) {
+                        _matrix->add(j, k,
+                                     _matrix->at(pivot_loc, k).value() *
+                                         _matrix->at(j, pivot_loc).value());
+                    }
+                }
+            }
+        }
+        // step 5
+        for (std::size_t pivot_row = 0; pivot_row < dim; pivot_row++) {
+            _matrix->modify(pivot_loc, pivot_row,
+                            _matrix->at(pivot_loc, pivot_row).value() / pivot);
+        }
+        _matrix->modify(pivot_loc, pivot_loc, Rational(1) / pivot); // step 8
+    }
+
+    for (const auto & [first_row, second_row] : row_swap_vec) {
+        _matrix->swap_rows(first_row, second_row);
+    }
+
+    _det = determinant;
+    if (is_transposed) {
+        transpose();
+    }
+}
 
 std::optional<Rational> Matrix::det() {
-    if (rows() != columns()){
+    if (rows() != columns()) {
         return std::nullopt;
     }
-    if (_det.has_value()){
+    if (_det.has_value()) {
         return _det;
     }
     _det = calc_det();
@@ -169,17 +256,17 @@ std::optional<Rational> Matrix::det() {
 }
 
 std::optional<Rational> Matrix::det() const {
-    if (rows() != columns()){
+    if (rows() != columns()) {
         return std::nullopt;
     }
-    if (_det.has_value()){
+    if (_det.has_value()) {
         return _det;
     }
     return calc_det();
 }
 
 std::size_t Matrix::rank() {
-    if (_rank.has_value()){
+    if (_rank.has_value()) {
         return _rank.value();
     }
     _rank = calc_rank();
@@ -187,7 +274,7 @@ std::size_t Matrix::rank() {
 }
 
 std::size_t Matrix::rank() const {
-    if (_rank.has_value()){
+    if (_rank.has_value()) {
         return _rank.value();
     }
     return calc_rank();
@@ -207,9 +294,8 @@ std::optional<Rational> Matrix::calc_det() const {
     Matrix copied_matrix(*this);
 
     std::vector<Rational> division_vec;
-    copied_matrix.gem_swap_rows([&](std::size_t, std::size_t) {
-        division_vec.emplace_back(-1);
-    });
+    copied_matrix.gem_swap_rows(
+        [&](std::size_t, std::size_t) { division_vec.emplace_back(-1); });
 
     copied_matrix.gem_row_elim([&](std::size_t i, std::size_t) {
         division_vec.emplace_back(copied_matrix._matrix->at(i, i).value());
