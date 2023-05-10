afa07ea918e623bf20188adaf572272020132ff0
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-10 16:32:46 +0200

    add OperatorPriority enum

--- src/Operator.h
+++ src/Operator.h
@@ -15,5 +15,31 @@ enum class Operator{
     IMPORT,
     EXPORT,
     PRINT,
-    SCAN
+    SCAN,
+    LEFT_PARENTHESIS,
+    RIGHT_PARENTHESIS,
+    MATRIX_BRACE_RIGHT,
+    MATRIX_BRACE_LEFT
+};
+
+enum class OperatorPriority{
+    PLUS = 2,
+    MINUS = 2,
+    ASSIGN = 0,
+    MUL = 3,
+    TRANSPOSE = 1,
+    UNITE = 1,
+    CUT = 1,
+    INV = 1,
+    DET = 1,
+    RANK = 1,
+    GAUSS = 1,
+    IMPORT = 1,
+    EXPORT = 1,
+    PRINT = 1,
+    SCAN = 1,
+    LEFT_PARENTHESIS = 100,
+    RIGHT_PARENTHESIS = 100,
+    MATRIX_BRACE_RIGHT = 100,
+    MATRIX_BRACE_LEFT = 100
 };
