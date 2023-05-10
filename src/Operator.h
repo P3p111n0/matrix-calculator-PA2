fdfcc5af6506c2712e7619c366b1c6512c724cca
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-10 13:59:59 +0200

    add Operator enum

---
+++ src/Operator.h
@@ -0,0 +1,19 @@
+#pragma once
+
+enum class Operator{
+    PLUS,
+    MINUS,
+    ASSIGN,
+    MUL,
+    TRANSPOSE,
+    UNITE,
+    CUT,
+    INV,
+    DET,
+    RANK,
+    GAUSS,
+    IMPORT,
+    EXPORT,
+    PRINT,
+    SCAN
+};
