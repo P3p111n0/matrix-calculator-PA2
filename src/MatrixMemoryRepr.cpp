4b67d11cc6f3eb2acea6dd800ece43e1564c07d5
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-21 00:24:40 +0200

    change ostream op method to print also numbers

--- src/MatrixMemoryRepr.cpp
+++ src/MatrixMemoryRepr.cpp
@@ -4,7 +4,11 @@ MatrixMemoryRepr::MatrixMemoryRepr(std::size_t rows, std::size_t columns)
     : _dimensions(rows, columns) {}
 
 std::ostream & operator<<(std::ostream & os, const MatrixMemoryRepr & matrix) {
-    matrix.print(os);
+    if (matrix.rows() == 1 && matrix.columns() == 1){
+        os << matrix.at(0, 0).value();
+    } else {
+        matrix.print(os);
+    }
     return os;
 }
 
