25c9c938540cd4dd53febd593542e5ab485285b5
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-22 19:48:20 +0200

    big evaluator changes, its radioactive now

--- src/Evaluator.cpp
+++ src/Evaluator.cpp
@@ -1,8 +1,11 @@
 #include "Evaluator.h"
 #include "Matrix.h"
+#include "Operator.h"
 #include "OperatorLookup.h"
+#include "ParsedInput.h"
+#include <functional>
+#include <stdexcept>
 #include <string>
-#include <vector>
 
 static inline constexpr char RESULT_NAME[] = "RESULT";
 
@@ -12,36 +15,70 @@ template <typename T> inline T top_and_pop(std::stack<T> & x) {
     return top;
 }
 
+void Evaluator::load_nontmp_vars(const Evaluator::VariableMap & tmps) {
+    for (const auto & [key, val] : tmps) {
+        if (!string_has_prefix(key, RESERVED_NAME_PREFIX)) {
+            if (_vars.count(key)) {
+                _stream << "Warning: Redefinition of variable: " << key
+                        << std::endl;
+                _vars.erase(key);
+            }
+            _vars.emplace(key, val);
+        }
+    }
+}
+
 Evaluator::Evaluator(MatrixFactory factory, std::ostream & os)
     : InputHandler(factory), _stream(os) {}
 
-bool Evaluator::evaluate_input(
-    Evaluator::VariableMap & variables,
-    Evaluator::OutputQueue & output_queue_pointer) const {
-    auto & output_queue = *output_queue_pointer;
+bool Evaluator::evaluate_input(const ParsedInput & input) {
+    auto & output_queue = *input.output_queue;
+    auto loaded_vars = *input.loaded_variables;
+    load_nontmp_vars(loaded_vars);
     std::stack<std::string> process_stack;
+
+    auto getter = [&](const std::string & key) {
+        if (_vars.count(key)) {
+            return _vars.at(key);
+        } else if (loaded_vars.count(key)) {
+            return loaded_vars.at(key);
+        }
+        throw std::runtime_error("Unknown token: " + key);
+    };
+
+    auto inserter = [&](const std::string & key, const Matrix & value) {
+        if (string_has_prefix(key, RESERVED_NAME_PREFIX)){
+            loaded_vars.erase(key);
+            loaded_vars.emplace(key, value);
+            return;
+        }
+        _vars.erase(key);
+        _vars.emplace(key, value);
+    };
+
+    ContainerOperations actions(getter, inserter);
 
     while (!output_queue.empty()) {
         std::string token = output_queue.front();
         output_queue.pop();
 
-        if (!(OPERATOR_LOOKUP.count(token) || FUNCTION_LOOKUP.count(token))) {
+        if (!OPERATOR_LOOKUP.count(token)) {
             process_stack.push(token);
             continue;
         }
 
-        Operator op = FULL_OPERATOR_LOOKUP.at(token);
+        Operator op = OPERATOR_LOOKUP.at(token);
         std::size_t n_of_args = N_OF_ARGS_LOOKUP.at(op);
 
         switch (n_of_args) {
         case 1:
-            handle_one_arg(process_stack, variables, op);
+            handle_one_arg(process_stack, op, actions);
             break;
         case 2:
-            handle_two_args(process_stack, variables, op);
+            handle_two_args(process_stack, op, actions);
             break;
         case 5:
-            handle_five_args(process_stack, variables, op);
+            handle_five_args(process_stack, op, actions);
             break;
         default:
             // this shouldn't happen
@@ -51,64 +88,49 @@ bool Evaluator::evaluate_input(
 
     if (!process_stack.empty()) {
         std::string leftover = process_stack.top();
-        Matrix res = variables.at(leftover);
+        Matrix res = getter(leftover);
         _stream << res << std::endl;
     }
-
-    remove_temporary_vars(variables);
     return true;
 }
 
 void Evaluator::handle_one_arg(std::stack<std::string> & process_stack,
-                               Evaluator::VariableMap & variables,
-                               Operator op) const {
+                               Operator op, const ContainerOperations & actions) {
     if (process_stack.empty()) {
         throw std::runtime_error(
             "Invalid number of arguments, 1 expected, 0 provided.");
     }
 
     std::string a = top_and_pop(process_stack);
-    if (!variables.count(a)) {
-        throw std::invalid_argument("Invalid token: " + a);
-    }
-
-    Matrix rhs = variables.at(a);
-    std::string result_name = get_temporary_name(RESULT_NAME, variables.size());
+    Matrix rhs = actions.get_var(a);
+    std::string result_name = get_temporary_name(RESULT_NAME, _vars.size());
 
     if (!process_stack.empty()) {
         result_name = a;
     }
 
-    if (op == Operator::DET) {
-        auto det = rhs.det();
-        if (det.has_value()) {
-            variables.emplace(result_name, Matrix(det.value(), _factory));
-            process_stack.emplace(result_name);
-            return;
-        }
-        throw std::runtime_error("Matrix " + a + "has no determinant");
-    }
-
-    if (op == Operator::RANK) {
-        auto rank = rhs.rank();
-        variables.emplace(result_name,
-                          Matrix(static_cast<double>(rank), _factory));
-        process_stack.push(result_name);
-        return;
-    }
-
     switch (op) {
+    case Operator::DET: {
+        auto det = rhs.det();
+        if (!det.has_value()) {
+            throw std::runtime_error("Determinant of " + a + "is undefined.");
+        }
+        rhs = Matrix(det.value(), _factory);
+        break;
+    }
+    case Operator::RANK: {
+        auto rank = rhs.rank();
+        rhs = Matrix(static_cast<double>(rank), _factory);
+        break;
+    }
     case Operator::TRANSPOSE:
         rhs.transpose();
-        process_stack.push(result_name);
         break;
     case Operator::INV:
         rhs.inverse();
-        process_stack.push(result_name);
         break;
     case Operator::GAUSS:
         rhs.gem();
-        process_stack.push(result_name);
         break;
     case Operator::IMPORT: // todo import
         break;
@@ -120,13 +142,13 @@ void Evaluator::handle_one_arg(std::stack<std::string> & process_stack,
     }
 
     if (op != Operator::PRINT) {
-        variables.erase(result_name);
-        variables.emplace(result_name, rhs);
+        actions.replace_var(result_name, rhs);
+        process_stack.push(result_name);
     }
 }
+
 void Evaluator::handle_two_args(std::stack<std::string> & process_stack,
-                                Evaluator::VariableMap & variables,
-                                Operator op) const {
+                                Operator op, const ContainerOperations & actions) {
     if (process_stack.size() < 2) {
         throw std::runtime_error("Invalid number of arguments, 2 expected " +
                                  std::to_string(process_stack.size()) +
@@ -136,51 +158,39 @@ void Evaluator::handle_two_args(std::stack<std::string> & process_stack,
     std::string b = top_and_pop(process_stack);
     std::string a = top_and_pop(process_stack);
 
-    if (!variables.count(b)) {
-        throw std::invalid_argument("Unknown token: " + b);
+    if (op == Operator::ASSIGN && !process_stack.empty()) {
+        throw std::runtime_error("Invalid use of '=' inside an expression.");
     }
 
-    std::string result_name = get_temporary_name(RESULT_NAME, variables.size());
+    std::string result_name = get_temporary_name(RESULT_NAME, _vars.size());
+    Matrix rhs = actions.get_var(b);
 
-    Matrix rhs = variables.at(b);
-    if (auto a_count = variables.count(a);
-        !a_count &&
-        op == Operator::ASSIGN) { // temporary for the ASSIGN operator
-        variables.emplace(a, Matrix(0, _factory));
-        result_name = a;
-    } else if (!a_count) {
-        throw std::invalid_argument("Unknown token: " + a);
+    if (op == Operator::ASSIGN){
+        actions.replace_var(a, rhs);
+        return;
     }
 
-    Matrix lhs = variables.at(a);
+    Matrix lhs = actions.get_var(a);
 
     switch (op) {
     case Operator::PLUS:
-        variables.emplace(result_name, lhs + rhs);
-        process_stack.push(result_name);
+        actions.replace_var(result_name, lhs + rhs);
         break;
     case Operator::MINUS:
-        variables.emplace(result_name, lhs - rhs);
-        process_stack.push(result_name);
-        break;
-    case Operator::ASSIGN:
-        variables.erase(result_name);
-        variables.emplace(result_name, rhs);
+        actions.replace_var(result_name, lhs - rhs);
         break;
     case Operator::MUL:
-        variables.emplace(result_name, lhs * rhs);
-        process_stack.push(result_name);
+        actions.replace_var(result_name, lhs * rhs);
         break;
     case Operator::UNITE:
-        variables.emplace(result_name, Matrix::unite(lhs, rhs));
-        process_stack.push(a);
+        actions.replace_var(result_name, Matrix::unite(lhs, rhs));
         break;
     }
+    process_stack.push(result_name);
 }
 
 void Evaluator::handle_five_args(std::stack<std::string> & process_stack,
-                                 Evaluator::VariableMap & variables,
-                                 Operator op) const {
+                                 Operator op, const ContainerOperations & actions) {
     if (process_stack.size() < 5) {
         throw std::runtime_error("Invalid number of arguments, 5 expected " +
                                  std::to_string(process_stack.size()) +
@@ -194,13 +204,13 @@ void Evaluator::handle_five_args(std::stack<std::string> & process_stack,
     std::string rhs_token = top_and_pop(process_stack);
 
     // might have to check for existing tokens
-    Matrix rhs = variables.at(rhs_token);
-    Matrix offset_row_matrix = variables.at(offset_row_token);
-    Matrix offset_column_matrix = variables.at(offset_column_token);
-    Matrix new_rows_matrix = variables.at(new_rows_token);
-    Matrix new_columns_matrix = variables.at(new_columns_token);
+    Matrix rhs = actions.get_var(rhs_token);
+    Matrix offset_row_matrix = actions.get_var(offset_row_token);
+    Matrix offset_column_matrix = actions.get_var(offset_column_token);
+    Matrix new_rows_matrix = actions.get_var(new_rows_token);
+    Matrix new_columns_matrix = actions.get_var(new_columns_token);
 
-    std::string result_name = get_temporary_name(RESULT_NAME, variables.size());
+    std::string result_name = get_temporary_name(RESULT_NAME, _vars.size());
     if (process_stack.empty()) {
         result_name = rhs_token;
     }
@@ -212,7 +222,6 @@ void Evaluator::handle_five_args(std::stack<std::string> & process_stack,
         break;
     }
 
-    variables.erase(result_name);
-    variables.emplace(result_name, rhs);
+    actions.replace_var(result_name, rhs);
     process_stack.emplace(result_name);
 }
--- src/Evaluator.h
+++ src/Evaluator.h
@@ -9,21 +9,30 @@
 #include "Matrix.h"
 #include "MatrixFactory.h"
 #include "Operator.h"
+#include "ParsedInput.h"
 
 class Evaluator : public InputHandler {
-    using OutputQueue = std::shared_ptr<std::queue<std::string>>;
-
+    using VariableMap = std::unordered_map<std::string, Matrix>;
   public:
     Evaluator(MatrixFactory, std::ostream &);
-    bool evaluate_input(VariableMap &, OutputQueue &) const;
+    bool evaluate_input(const ParsedInput &);
 
   private:
     std::ostream & _stream;
+    VariableMap _vars;
 
-    void handle_one_arg(std::stack<std::string> &, VariableMap &,
-                        Operator) const;
-    void handle_two_args(std::stack<std::string> &, VariableMap &,
-                         Operator) const;
-    void handle_five_args(std::stack<std::string> &, VariableMap &,
-                          Operator) const;
+    struct ContainerOperations {
+        ContainerOperations(
+            const std::function<Matrix(const std::string &)> & getter,
+            const std::function<void(const std::string &, const Matrix &)> &
+                replacer)
+            : get_var(getter), replace_var(replacer) {}
+        std::function<Matrix(const std::string &)> get_var;
+        std::function<void(const std::string &, const Matrix &)> replace_var;
+    };
+
+    void handle_one_arg(std::stack<std::string> &, Operator, const ContainerOperations &);
+    void handle_two_args(std::stack<std::string> &, Operator, const ContainerOperations &);
+    void handle_five_args(std::stack<std::string> &, Operator, const ContainerOperations &);
+    void load_nontmp_vars(const VariableMap &);
 };
