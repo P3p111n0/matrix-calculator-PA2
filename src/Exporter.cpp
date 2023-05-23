02adf5e3538f511d4f32bb3cd85d3344ac1770a7
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-23 15:18:51 +0200

    export methods done

--- src/Exporter.cpp
+++ src/Exporter.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <iomanip>
 
 using json = nlohmann::json;
 
@@ -10,7 +11,7 @@ Exporter::Exporter(MatrixFactory factory) : FileHandler(factory) {}
 
 static void write_sparse(json & file, const std::string & name, const Matrix & mx) {
     for (const auto & [pos, val] : mx) {
-        std::string format = std::to_string(pos.row) + " - " + std::to_string(pos.column);
+        std::string format = std::to_string(pos.row) + ":" + std::to_string(pos.column);
         file[name]["data"].emplace(format, val);
     }
 }
@@ -18,7 +19,7 @@ static void write_sparse(json & file, const std::string & name, const Matrix & m
 static void write_dense(json & file, const std::string & name, const Matrix & mx) {
     std::vector<std::vector<double>> data_vec(mx.rows());
     for (auto & row : data_vec){
-        row.reserve(mx.columns());
+        row.resize(mx.columns());
     }
     for (const auto & [pos, val] : mx){
         data_vec[pos.row][pos.column] = val;
@@ -43,15 +44,19 @@ void Exporter::export_to_file(const std::unordered_map<std::string, Matrix> & va
         std::size_t number_of_non_zeroes = matrix.begin().distance(matrix.end());
         std::size_t max_non_zeroes = (1 - _factory.ratio()) * matrix.rows() * matrix.columns();
 
+        out_data[key]["data"];
         if (number_of_non_zeroes < max_non_zeroes){
             write_sparse(out_data, key, matrix);
         } else {
             write_dense(out_data, key, matrix);
         }
     }
-    outfile << out_data;
+    outfile << std::setw(3) << out_data;
     if (!outfile.is_open() || outfile.bad()){
         _status = "Error while writing data.";
         _is_failed = true;
+    } else {
+        _status = "Write to " + filename + " finished successfully.";
+        _is_failed = false;
     }
 }
