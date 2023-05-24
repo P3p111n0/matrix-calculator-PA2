4113e5140d2148f9c8d1448c6b625de1e5bba0ed
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-24 15:58:48 +0200

    add priority_of method, refactor

--- src/matrix_operations/OperationFactory.cpp
+++ src/matrix_operations/OperationFactory.cpp
@@ -1,4 +1,5 @@
 #include "OperationFactory.h"
+#include "MatrixOp.h"
 #include "Operator.h"
 #include "five_args/MatrixOpCut.h"
 #include "single_argument/MatrixOpDet.h"
@@ -10,31 +11,29 @@
 #include "two_args/MatrixOpMul.h"
 #include "two_args/MatrixOpPlus.h"
 #include "two_args/MatrixOpUnite.h"
-#include <stdexcept>
+#include <memory>
 
 bool OperationCmp::operator()(const std::shared_ptr<MatrixOp> & lhs,
                               const std::shared_ptr<MatrixOp> & rhs) const {
     return lhs->name() < rhs->name();
 }
 
-inline const std::unordered_map<std::string, Operator> lookup_map = {
-    {"+", Operator::PLUS},
-    {"-", Operator::MINUS},
-    {"*", Operator::MUL},
-    {"UNITE", Operator::UNITE},
-    {"CUT", Operator::CUT},
-    {"TRANSPOSE", Operator::TRANSPOSE},
-    {"INV", Operator::INV},
-    {"DET", Operator::DET},
-    {"RANK", Operator::RANK},
-    {"GAUSS", Operator::GAUSS},
-};
-
-OperationFactory::OperationFactory() : _operations(lookup_map) {}
+OperationFactory::OperationFactory() {
+    _operations.emplace("+", Operator::PLUS);
+    _operations.emplace("-", Operator::MINUS);
+    _operations.emplace("*", Operator::MUL);
+    _operations.emplace("UNITE", Operator::UNITE);
+    _operations.emplace("CUT", Operator::CUT);
+    _operations.emplace("TRANSPOSE", Operator::TRANSPOSE);
+    _operations.emplace("INV", Operator::INV);
+    _operations.emplace("DET", Operator::DET);
+    _operations.emplace("RANK", Operator::RANK);
+    _operations.emplace("GAUSS", Operator::GAUSS);
+}
 
 MatrixOp * OperationFactory::get_operation(const std::string & op_name) const {
-    if (!_operations.count(op_name)){
-        //this shouldn't happen
+    if (!_operations.count(op_name)) {
+        // this shouldn't happen
         throw std::invalid_argument("Unknown operation name: " + op_name);
     }
     Operator op = _operations.at(op_name);
@@ -60,7 +59,7 @@ MatrixOp * OperationFactory::get_operation(const std::string & op_name) const {
     case Operator::GAUSS:
         return new MatrixOpGauss;
     default:
-        //this definitely shouldn't happen
+        // this definitely shouldn't happen
         throw std::runtime_error("No matching operator for: " + op_name);
     }
 }
@@ -68,7 +67,7 @@ MatrixOp * OperationFactory::get_operation(const std::string & op_name) const {
 std::set<std::shared_ptr<MatrixOp>, OperationCmp>
 OperationFactory::get_all_operations() const {
     std::set<std::shared_ptr<MatrixOp>, OperationCmp> op_set;
-    for (const auto & [key, op] : _operations){
+    for (const auto & [key, op] : _operations) {
         op_set.emplace(get_operation(key));
     }
     return op_set;
@@ -76,4 +75,9 @@ OperationFactory::get_all_operations() const {
 
 bool OperationFactory::is_operation(const std::string & name) const {
     return _operations.count(name);
+}
+
+std::size_t OperationFactory::priority_of(const std::string & op) const {
+    std::unique_ptr<MatrixOp> operation(get_operation(op));
+    return operation->priority();
 }
--- src/matrix_operations/OperationFactory.h
+++ src/matrix_operations/OperationFactory.h
@@ -17,6 +17,7 @@ class OperationFactory {
     MatrixOp * get_operation(const std::string &) const;
     std::set<std::shared_ptr<MatrixOp>, OperationCmp> get_all_operations() const;
     bool is_operation(const std::string &) const;
+    std::size_t priority_of(const std::string &) const;
   private:
     std::unordered_map<std::string, Operator> _operations;
 };
