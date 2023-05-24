87f801cef78ab96f72ed2a8f2b483a3ed60ffa5d
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-24 14:46:56 +0200

    remove useless operation

--- src/handlers/input_handling/ContainerOperations.h
+++ src/handlers/input_handling/ContainerOperations.h
@@ -5,11 +5,8 @@
 #include "../../matrix_wrapper/Matrix.h"
 
 struct ContainerOperations {
-    ContainerOperations(
-        const std::function<Matrix &(const std::string &)> & getter,
-        const std::function<void(const std::string &, const Matrix &)> &
-            replacer)
-        : get_var(getter), replace_var(replacer) {}
+    explicit ContainerOperations(
+        const std::function<Matrix &(const std::string &)> & getter)
+        : get_var(getter) {}
     std::function<Matrix &(const std::string &)> get_var;
-    std::function<void(const std::string &, const Matrix &)> replace_var;
 };
