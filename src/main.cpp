18794a1653c4b36dd5c6830dc3b6096250853280
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-25 18:31:02 +0200

    change return values in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 int main(int argc, char * argv[]){
     if (argc > 2){
         std::cerr << "Invalid number of command line arguments." << std::endl;
-        return 0;
+        return EXIT_FAILURE;
     }
     try {
         MatrixCalculator calculator(std::cin, std::cout, argc == 2 ? argv[1] : "");
         calculator.start();
     } catch (std::exception & e){
         std::cerr << e.what() << std::endl;
-        return 0;
+        return EXIT_FAILURE;
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
