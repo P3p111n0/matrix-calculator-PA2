e90fec248bfdbebca0b6624964ccdffe43309f92
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-26 14:10:26 +0200

    add docs for five arg operations

--- src/matrix_operations/five_args/MatrixOpCut.h
+++ src/matrix_operations/five_args/MatrixOpCut.h
@@ -2,8 +2,28 @@
 
 #include "MatrixOpFiveArgs.h"
 
+/**
+ * @brief An operation representing sub-matrix extraction.
+ */
 class MatrixOpCut : public MatrixOpFiveArgs {
   public:
+
+    /**
+     * @brief Initializes the operator to <b>priority = 3</b> and
+     *        <b>name = "CUT"</b>.
+     */
     MatrixOpCut();
-    Matrix evaluate(const std::vector<Matrix> &) const override;
+
+    /**
+     * @brief Performs the sub-matrix extraction.
+     *        Calls <b>Matrix::cut(const Matrix &, const Matrix &,
+     *        const Matrix &, const Matrix &)</b>.
+     * @param args An array of arguments for sub-matrix extraction. For
+     *             requirements for <b>args</b>, see <b>MatrixOp::evaluate()</b>.
+     * @return The extracted sub-matrix.
+     * @throws std::invalid_argument if <b>Matrix::cut(const Matrix &,
+     *                               const Matrix &, const Matrix &,
+     *                               const Matrix &)</b> throws.
+     */
+    Matrix evaluate(const std::vector<Matrix> & args) const override;
 };
--- src/matrix_operations/five_args/MatrixOpFiveArgs.h
+++ src/matrix_operations/five_args/MatrixOpFiveArgs.h
@@ -2,6 +2,10 @@
 
 #include "../MatrixOp.h"
 
+/**
+ * @brief An abstract base class for operations with 5 arguments. Initializes
+ *        <b>MatrixOp::arity</b> to 5.
+ */
 class MatrixOpFiveArgs : public MatrixOp {
   protected:
     MatrixOpFiveArgs(std::size_t priority, const std::string & name)
