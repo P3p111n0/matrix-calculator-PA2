477dcbf53180b60e28edb908b33cb28ca6d2cf91
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-26 14:18:39 +0200

    add docs for special cases

--- src/matrix_operations/special_cases/MatrixOpAssign.h
+++ src/matrix_operations/special_cases/MatrixOpAssign.h
@@ -2,8 +2,23 @@
 
 #include "../MatrixOp.h"
 
+/**
+ * @brief Represents a special case for matrix operations. Classes in the
+ *        <b>special_cases</b> directory do nothing, they only exist for
+ *        the purposes of parsing.
+ */
 class MatrixOpAssign : public MatrixOp {
   public:
+
+    /**
+     * @brief Initializes the operator to <b>arity = 2</b>, <b>priority = 0</b>
+     *        and <b>name = "="</b>.
+     */
     MatrixOpAssign();
+
+    /**
+     * @brief Only exists for the purposes of parsing.
+     * @return Value 0 in a 1x1 matrix regardless of it's arguments.
+     */
     Matrix evaluate(const std::vector<Matrix> &) const override;
 };
--- src/matrix_operations/special_cases/MatrixOpExport.h
+++ src/matrix_operations/special_cases/MatrixOpExport.h
@@ -2,8 +2,23 @@
 
 #include "../MatrixOp.h"
 
+/**
+ * @brief Represents a special case for matrix operations. Classes in the
+ *        <b>special_cases</b> directory do nothing, they only exist for
+ *        the purposes of parsing.
+ */
 class MatrixOpExport : public MatrixOp {
   public:
+
+    /**
+     * @brief Initializes the operation to <b>arity = 1</b>, <b>priority = 0</b>
+     *        and <b>name = "EXPORT"</b>.
+     */
     MatrixOpExport();
+
+    /**
+     * @brief Only exists for the purposes of parsing.
+     * @return Value 0 in a 1x1 matrix regardless of it's arguments.
+     */
     Matrix evaluate(const std::vector<Matrix> &) const override;
 };
