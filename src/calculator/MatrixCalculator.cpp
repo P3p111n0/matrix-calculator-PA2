0754ba5d97b4884352a0d2faa4c600d01b5fd728
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-27 17:56:34 +0200

    add error prefix

--- src/calculator/MatrixCalculator.cpp
+++ src/calculator/MatrixCalculator.cpp
@@ -13,17 +13,20 @@ void MatrixCalculator::start() {
     Parser parser(MatrixFactory(_config.sparse_ratio), _in,
                   _config.max_input_length);
     Evaluator evaluator(MatrixFactory(_config.sparse_ratio), _out);
+    std::string prefix;
     while (!_in.eof()) {
         if (!_in.good()){
             _out << "Corrupted data detected on input, quitting." << std::endl;
             return;
         }
         try {
-            _out << ">>> ";
+            _out << prefix << ">>> ";
             auto parsed_input = parser.parse_input();
             evaluator.evaluate_input(parsed_input);
+            prefix.clear();
         } catch (std::exception & e){
             _out << e.what() << std::endl;
+            prefix = "!**";
             continue;
         }
     }
