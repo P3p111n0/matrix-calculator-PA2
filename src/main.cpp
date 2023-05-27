1c715f789c860ca7df5f69a819124cbaf4b20bd4
Josef Melcr <melcrjos@fit.cvut.cz>
2023-05-27 23:33:57 +0200

    add signal handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
 #include "calculator/MatrixCalculator.h"
+#include <csignal>
+#include <iostream>
+
+//http://vyuka.bernhauer.cz/pa2-clanky/semestralni-prace
+void signal_handler(int) {
+    exit(0);
+}
 
 int main(int argc, char * argv[]){
     if (argc > 2){
         std::cerr << "Invalid number of command line arguments." << std::endl;
         return EXIT_FAILURE;
     }
+
+    std::signal(SIGTERM, signal_handler);
+    std::signal(SIGINT, signal_handler);
+
     try {
         MatrixCalculator calculator(std::cin, std::cout, argc == 2 ? argv[1] : "");
         calculator.start();
